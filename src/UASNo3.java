3626154947600fd86b33bedc662e59b0346e8459
Dede Fernanda <143549355+ddfrnnd@users.noreply.github.com>
2024-01-16 12:14:39 +0700

    UASNo3.java
    SOAL3

--- src/UASNo3.java
+++ src/UASNo3.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 class Pensil {
@@ -9,24 +11,66 @@ class Pensil {
 }
 
 public class UASNo3 {
-
     public static void main(String[] args) {
         Random random = new Random();
-        int totalPensil = 20000;
-        int[] jumlahProduksi = new int[6];
-        int maxProduksi = 0;
-        int minProduksi = totalPensil;
-        Pensil[] pensil = new Pensil[6];
+        int totalProduksi = 20000;
+        ArrayList<Pensil> bluePensils = new ArrayList<Pensil>();
+        ArrayList<Pensil> redPensils = new ArrayList<Pensil>();
+        ArrayList<Pensil> greenPensils = new ArrayList<Pensil>();
+        ArrayList<Pensil> orangePensils = new ArrayList<Pensil>();
+        ArrayList<Pensil> blackPensils = new ArrayList<Pensil>();
+        ArrayList<Pensil> purplePensils = new ArrayList<Pensil>();
 
-        for (int i = 0; i < 6; i++) {
-            pensil[i] = new Pensil("warna" + i);
-            jumlahProduksi[i] = random.nextInt(totalPensil / 2) + totalPensil / 4;
-            maxProduksi = Math.max(maxProduksi, jumlahProduksi[i]);
-            minProduksi = Math.min(minProduksi, jumlahProduksi[i]);
-            System.out.println("Jumlah produksi " + pensil[i].warna + " : " + jumlahProduksi[i]);
+        for (int i = 0; i < totalProduksi; i++) {
+            int randomWarna = random.nextInt(6);
+            switch (randomWarna) {
+                case 0:
+                    bluePensils.add(new Pensil("biru"));
+                    break;
+                case 1:
+                    redPensils.add(new Pensil("merah"));
+                    break;
+                case 2:
+                    greenPensils.add(new Pensil("hijau"));
+                    break;
+                case 3:
+                    orangePensils.add(new Pensil("oranye"));
+                    break;
+                case 4:
+                    blackPensils.add(new Pensil("hitam"));
+                    break;
+                case 5:
+                    purplePensils.add(new Pensil("ungu"));
+                    break;
+            }
         }
 
-        System.out.println("Jumlah produksi maksimum : " + maxProduksi);
-        System.out.println("Jumlah produksi minimum : " + minProduksi);
+        System.out.println("Total jumlah pensil dari setiap warna:");
+        System.out.println("Biru: " + bluePensils.size());
+        System.out.println("Merah: " + redPensils.size());
+        System.out.println("Hijau: " + greenPensils.size());
+        System.out.println("Oranye: " + orangePensils.size());
+        System.out.println("Hitam: " + blackPensils.size());
+        System.out.println("Ungu: " + purplePensils.size());
+
+        int maxProduksi = 0;
+        int minProduksi = Integer.MAX_VALUE;
+        String maxWarna = "";
+        String minWarna = "";
+
+        for (ArrayList<Pensil> pensils : Arrays.asList(bluePensils, redPensils, greenPensils, orangePensils, blackPensils, purplePensils)) {
+            int produksi = pensils.size();
+            if (produksi > maxProduksi) {
+                maxProduksi = produksi;
+                maxWarna = pensils.get(0).warna;
+            }
+            if (produksi < minProduksi) {
+                minProduksi = produksi;
+                minWarna = pensils.get(0).warna;
+            }
+        }
+
+        System.out.println("Jumlah produksi maksimum: " + maxProduksi + " (" + maxWarna + ")");
+        System.out.println("Jumlah produksi minimum: " + minProduksi + " (" + minWarna + ")");
     }
 }
